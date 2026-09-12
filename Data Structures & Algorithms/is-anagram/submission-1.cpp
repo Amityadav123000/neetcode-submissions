5a3c6408666c1da6feeb040b20768ab04a14c900
Amityadav123000 <66779416+Amityadav123000@users.noreply.github.com>
2026-09-12 13:01:10 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> map;
+
+        for(auto ch:s){
+            map[ch]++;
+        }
+
+        for(auto ch:t){
+            map[ch]--;
+            if(map[ch]==0){
+                map.erase(ch);
+            }
+        }
+
+        if(!map.empty()){
+            return false;
+        }
+
+        return true;
+    }
+};
